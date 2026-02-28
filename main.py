14c3e7e836a059a73488fccdcf5e12b43536127a
camillelaporte75015-arch <camille.laporte75015@gmail.com>
2026-02-28 09:50:58 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,313 @@
+import asyncio
+import random
+import time
+from telethon import TelegramClient, events
+from flask import Flask
+import threading
+import requests
+
+# --- CONFIG ---
+API_ID = 36767235
+API_HASH = "6a36bf6c4b15e7eecdb20885a13fc2d7"
+BOT_TOKEN = "8640125594:AAG9sZVyTCt3J9-sK2aEC8yLR5Az_YDBSMk"
+OWNER_ID = 7891919458  # seul cet ID peut gérer la BL
+GIPHY_API_KEY = "YLLksuIyKHZcaMKuAOYR1s27dz2uy8Xr"
+
+# --- STRUCTURES ---
+blacklist = {OWNER_ID}  # Seuls les utilisateurs ici peuvent utiliser les commandes
+cooldowns = {}
+
+# --- TELETHON ---
+client = TelegramClient('bot_hentai', API_ID, API_HASH)
+
+# --- FLASK POUR UPTIMEROBOT ---
+app = Flask("")
+
+@app.route("/")
+def home():
+    return "Bot hentai/slap/kiss en ligne !"
+
+def run_flask():
+    app.run(host="0.0.0.0", port=5000)
+
+threading.Thread(target=run_flask).start()
+
+# --- UTILS ---
+COOLDOWN = 5  # secondes
+
+def is_on_cooldown(user_id):
+    return user_id in cooldowns and time.time() - cooldowns[user_id] < COOLDOWN
+
+def cooldown_remaining(user_id):
+    if user_id not in cooldowns:
+        return 0
+    return max(0, int(COOLDOWN - (time.time() - cooldowns[user_id])))
+
+def update_cooldown(user_id):
+    cooldowns[user_id] = time.time()
+
+def is_allowed(user_id):
+    return user_id in blacklist
+
+def get_giphy(tag):
+    url = f"https://api.giphy.com/v1/gifs/random?api_key={GIPHY_API_KEY}&tag={tag}&rating=pg-13"
+    resp = requests.get(url).json()
+    return resp['data']['images']['original']['url']
+
+# --- LIENS HENTAI ---
+hentai_gifs = [
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd459eaf877.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd459f14394.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd459f6d267.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd459fc75eb.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd45a02bca8.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd45a084d62.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd45a0ddb2f.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd45a1423d8.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd45a19b1cb.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd45a1f3e10.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd45a258610.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd45a2b141b.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd45a3168ea.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd45a36f3fd.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd45a3c845d.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd45a42b35a.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd45a484425.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd45a4dcad1.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd45a5415e3.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd45a598901.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd45a5f1ec8.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd45a656046.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd45a6af1aa.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd45a712990.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd45a769d40.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd45a7c1a1c.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd45a825a91.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd45a87d47b.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd45a938976.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd45a9903a5.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd45a9e8c1f.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd45aa4d0de.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd45aaa711e.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd45ab0b8ee.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd45ab635cf.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd45abbc4c0.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd45ac20f25.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd45ac786a5.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd45acd0644.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd45ad346c7.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd45ad8c3fd.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd45ade45d4.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd45ae47e32.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd45ae9f066.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd45af02a47.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd45af5b435.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd45afb3c6c.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd45b017ac6.gif",
+"https://s2.pictoa.com/media/galleries/296/760/2967605ffd459cac8ec/38322265ffd45b070efd.gif",
+"https://www.cougarillo.com/wp-content/uploads/2023/12/porno-hentai.gif",
+"https://www.cougarillo.com/wp-content/uploads/2023/12/levrette-gif-hentai.gif",
+"https://www.cougarillo.com/wp-content/uploads/2023/12/gif-hentai-gros-seins.gif",
+"https://www.cougarillo.com/wp-content/uploads/2024/04/gif-hentai145.gif",
+"https://www.cougarillo.com/wp-content/uploads/2024/04/gif-hentai125.gif",
+"https://www.cougarillo.com/wp-content/uploads/2024/04/gif-hentai143.gif",
+"https://www.cougarillo.com/wp-content/uploads/2024/04/gif-hentai129.gif",
+"https://www.cougarillo.com/wp-content/uploads/2024/04/gif-hentai119.gif",
+"https://www.cougarillo.com/wp-content/uploads/2024/04/gif-hentai127.gif",
+"https://www.cougarillo.com/wp-content/uploads/2024/04/gif-hentai122.gif",
+"https://www.cougarillo.com/wp-content/uploads/2024/04/gif-hentai120.gif",
+"https://www.cougarillo.com/wp-content/uploads/2024/04/gif-hentai141.gif",
+"https://www.cougarillo.com/wp-content/uploads/2024/04/gif-hentai139.gif",
+"https://www.cougarillo.com/wp-content/uploads/2024/04/gif-hentai140.gif",
+"https://www.cougarillo.com/wp-content/uploads/2024/04/gif-hentai114.gif",
+"https://www.cougarillo.com/wp-content/uploads/2024/04/gif-hentai123.gif",
+"https://www.cougarillo.com/wp-content/uploads/2024/04/gif-hentai115.gif",
+"https://www.cougarillo.com/wp-content/uploads/2024/04/gif-hentai142.gif",
+"https://img2.gelbooru.com//images/40/5f/405f442f0a5b6631821708238aed7d9a.gif",
+"https://img2.gelbooru.com//images/32/44/324418be5fba84ca057ce3601b944292.gif",
+"https://img2.gelbooru.com//images/70/09/7009626c5baad944ab31565e9509109a.gif",
+"https://commentseduire.net/wp-content/uploads/2017/06/hentai-gif-41.gif",
+"https://commentseduire.net/wp-content/uploads/2017/06/hentai-gif-40.gif",
+"https://commentseduire.net/wp-content/uploads/2017/06/hentai-gif-39.gif",
+"https://commentseduire.net/wp-content/uploads/2017/06/hentai-gif-38.gif",
+"https://commentseduire.net/wp-content/uploads/2017/06/hentai-gif-37.gif",
+"https://commentseduire.net/wp-content/uploads/2017/06/hentai-gif-36.gif",
+"https://commentseduire.net/wp-content/uploads/2017/06/hentai-gif-35.gif",
+"https://commentseduire.net/wp-content/uploads/2017/06/hentai-gif-34.gif",
+"https://commentseduire.net/wp-content/uploads/2017/06/hentai-gif-32.gif",
+"https://commentseduire.net/wp-content/uploads/2017/06/hentai-gif-30.gif",
+"https://commentseduire.net/wp-content/uploads/2017/06/hentai-gif-29.gif",
+"https://commentseduire.net/wp-content/uploads/2017/06/hentai-gif-28.gif",
+"https://commentseduire.net/wp-content/uploads/2017/06/hentai-gif-27.gif",
+"https://commentseduire.net/wp-content/uploads/2017/06/hentai-gif-25.gif",
+"https://commentseduire.net/wp-content/uploads/2017/06/hentai-gif-24.gif",
+"https://commentseduire.net/wp-content/uploads/2017/06/hentai-gif-23.gif",
+"https://commentseduire.net/wp-content/uploads/2017/06/hentai-gif-22.gif",
+"https://commentseduire.net/wp-content/uploads/2017/06/hentai-gif-21.gif",
+"https://commentseduire.net/wp-content/uploads/2017/06/hentai-gif-20.gif",
+"https://commentseduire.net/wp-content/uploads/2017/06/hentai-gif-19.gif",
+"https://commentseduire.net/wp-content/uploads/2017/06/hentai-gif-18.gif",
+"https://commentseduire.net/wp-content/uploads/2017/06/hentai-gif-17.gif",
+"https://commentseduire.net/wp-content/uploads/2017/06/hentai-gif-16.gif",
+"https://commentseduire.net/wp-content/uploads/2017/06/hentai-gif-15.gif",
+"https://commentseduire.net/wp-content/uploads/2017/06/hentai-gif-14.gif",
+"https://commentseduire.net/wp-content/uploads/2017/06/hentai-gif-13.gif",
+"https://commentseduire.net/wp-content/uploads/2017/06/hentai-gif-12.gif",
+"https://commentseduire.net/wp-content/uploads/2017/06/hentai-gif-11.gif",
+"https://commentseduire.net/wp-content/uploads/2017/06/hentai-gif-10.gif",
+"https://commentseduire.net/wp-content/uploads/2017/06/hentai-gif-9.gif",
+"https://commentseduire.net/wp-content/uploads/2017/06/hentai-gif-7.gif",
+"https://commentseduire.net/wp-content/uploads/2017/06/hentai-gif-6.gif",
+"https://commentseduire.net/wp-content/uploads/2017/06/hentai-gif-5.gif",
+"https://commentseduire.net/wp-content/uploads/2017/06/hentai-gif-4.gif",
+"https://commentseduire.net/wp-content/uploads/2017/06/hentai-gif-1.gif",
+ "https://wetgif.com/wp-content/uploads/hentai-1.gif",
+ "https://m1.hentaiera.com/002/wrmde0u65c/1.gif",
+    "https://m1.hentaiera.com/002/wrmde0u65c/2.gif",
+    "https://m1.hentaiera.com/002/wrmde0u65c/3.gif",
+    "https://m1.hentaiera.com/002/wrmde0u65c/4.gif",
+    "https://m1.hentaiera.com/002/wrmde0u65c/5.gif",
+    "https://m1.hentaiera.com/002/wrmde0u65c/6.gif",
+    "https://m1.hentaiera.com/002/wrmde0u65c/7.gif",
+    "https://m1.hentaiera.com/002/wrmde0u65c/8.gif",
+    "https://m1.hentaiera.com/002/wrmde0u65c/9.gif",
+    "https://m1.hentaiera.com/002/wrmde0u65c/12.gif",
+    "https://m1.hentaiera.com/002/wrmde0u65c/13.gif",
+    "https://m1.hentaiera.com/002/wrmde0u65c/15.gif",
+    "https://m1.hentaiera.com/002/wrmde0u65c/17.gif",
+    "https://m1.hentaiera.com/002/wrmde0u65c/18.gif",
+    "https://m1.hentaiera.com/002/wrmde0u65c/19.gif",
+    "https://m1.hentaiera.com/002/wrmde0u65c/20.gif",
+    "https://m1.hentaiera.com/002/wrmde0u65c/21.gif",
+    "https://m1.hentaiera.com/002/wrmde0u65c/22.gif",
+    "https://m1.hentaiera.com/002/wrmde0u65c/23.gif",
+    "https://m1.hentaiera.com/002/wrmde0u65c/24.gif",
+    "https://m1.hentaiera.com/002/wrmde0u65c/25.gif",
+    "https://m1.hentaiera.com/002/wrmde0u65c/26.gif",
+    "https://m1.hentaiera.com/002/wrmde0u65c/28.gif",
+    "https://m1.hentaiera.com/002/wrmde0u65c/30.gif",
+    "https://m1.hentaiera.com/002/wrmde0u65c/31.gif",
+    "https://m1.hentaiera.com/002/wrmde0u65c/32.gif",
+    "https://m1.hentaiera.com/002/wrmde0u65c/33.gif",
+    "https://m1.hentaiera.com/002/wrmde0u65c/34.gif",
+    "https://m1.hentaiera.com/002/wrmde0u65c/35.gif",
+    "https://m1.hentaiera.com/002/wrmde0u65c/36.gif",
+    "https://m1.hentaiera.com/002/wrmde0u65c/37.gif",
+    "https://m1.hentaiera.com/002/wrmde0u65c/38.gif",
+    "https://m1.hentaiera.com/002/wrmde0u65c/39.gif",
+    "https://m1.hentaiera.com/002/wrmde0u65c/40.gif",
+    "https://m1.hentaiera.com/002/wrmde0u65c/41.gif",
+    "https://m1.hentaiera.com/002/wrmde0u65c/42.gif",
+    "https://m1.hentaiera.com/002/wrmde0u65c/43.gif",
+    "https://m1.hentaiera.com/002/wrmde0u65c/44.gif",
+    "https://m1.hentaiera.com/002/wrmde0u65c/45.gif",
+    "https://m1.hentaiera.com/002/wrmde0u65c/46.gif",
+    "https://m4.hentaiera.com/013/zbl8pwx6im/1.gif",
+    "https://m2.hentaiera.com/009/wzchv6ndpy/1.gif",
+    "https://m2.hentaiera.com/009/wzchv6ndpy/3.gif",
+    "https://m2.hentaiera.com/009/wzchv6ndpy/4.gif",
+    "https://m2.hentaiera.com/009/wzchv6ndpy/7.gif",
+    "https://m2.hentaiera.com/009/wzchv6ndpy/8.gif",
+    "https://m2.hentaiera.com/009/wzchv6ndpy/9.gif",
+    "https://m2.hentaiera.com/009/wzchv6ndpy/10.gif",
+    "https://m2.hentaiera.com/009/wzchv6ndpy/11.gif",
+    "https://m2.hentaiera.com/009/wzchv6ndpy/14.gif",
+    "https://m2.hentaiera.com/009/wzchv6ndpy/16.gif",
+    "https://m2.hentaiera.com/009/wzchv6ndpy/19.gif",
+    "https://m2.hentaiera.com/009/wzchv6ndpy/21.gif",
+    "https://m2.hentaiera.com/009/wzchv6ndpy/22.gif",
+    "https://m2.hentaiera.com/009/wzchv6ndpy/27.gif",
+    "https://m2.hentaiera.com/009/wzchv6ndpy/29.gif",
+    "https://m2.hentaiera.com/009/wzchv6ndpy/30.gif"
+]
+
+# --- COMMANDES ---
+
+# .hentai
+@client.on(events.NewMessage(pattern=r'\.hentai'))
+async def cmd_hentai(event):
+    user_id = event.sender_id
+    if not is_allowed(user_id):
+        await event.reply("❌ Tu n'as pas le droit d'utiliser cette commande !")
+        return
+
+    if is_on_cooldown(user_id):
+        await event.reply(f"😡 Calme toi avec le bot attends {cooldown_remaining(user_id)}s")
+        return
+
+    update_cooldown(user_id)
+    gif = random.choice(hentai_gifs)
+    msg = await event.respond(gif)
+    await asyncio.sleep(7)
+    await msg.delete()
+
+# .kiss et .slap
+@client.on(events.NewMessage(pattern=r'\.(kiss|slap) @(\w+)'))
+async def cmd_action(event):
+    user_id = event.sender_id
+    if not is_allowed(user_id):
+        await event.reply("❌ Tu n'as pas le droit d'utiliser cette commande !")
+        return
+
+    if is_on_cooldown(user_id):
+        await event.reply(f"😡 Calme toi avec le bot attends {cooldown_remaining(user_id)}s")
+        return
+
+    update_cooldown(user_id)
+    action_type = event.pattern_match.group(1)
+    target_username = event.pattern_match.group(2)
+    initiator = event.sender.username or f"user{user_id}"
+    emoji = "💋" if action_type == "kiss" else "👋"
+    if action_type == "kiss":
+        gif = get_giphy("anime kiss")
+    else:
+        gif = get_giphy("anime slap")
+    await event.respond(f"@{initiator} {action_type} @{target_username} {emoji}\n{gif}")
+
+# .bl pour ajouter à la BL
+@client.on(events.NewMessage(pattern=r'\.bl (@?\w+|\d+)'))
+async def cmd_bl(event):
+    if event.sender_id != OWNER_ID:
+        await event.reply("❌ Tu n'as pas le droit d'utiliser cette commande !")
+        return
+    target = event.pattern_match.group(1).lstrip("@")
+    try:
+        target_id = int(target)
+    except:
+        target_id = target
+    blacklist.add(target_id)
+    await event.reply(f"✅ {target} ajouté à la BL.")
+
+# .unbl pour retirer
+@client.on(events.NewMessage(pattern=r'\.unbl (@?\w+|\d+)'))
+async def cmd_unbl(event):
+    if event.sender_id != OWNER_ID:
+        await event.reply("❌ Tu n'as pas le droit d'utiliser cette commande !")
+        return
+    target = event.pattern_match.group(1).lstrip("@")
+    try:
+        target_id = int(target)
+    except:
+        target_id = target
+    blacklist.discard(target_id)
+    await event.reply(f"✅ {target} retiré de la BL.")
+
+# .blacklist pour afficher
+@client.on(events.NewMessage(pattern=r'\.blacklist'))
+async def cmd_blacklist(event):
+    if event.sender_id != OWNER_ID:
+        await event.reply("❌ Tu n'as pas le droit d'utiliser cette commande !")
+        return
+    if not blacklist:
+        await event.reply("✅ La BL est vide.")
+    else:
+        users = ", ".join(str(u) for u in blacklist)
+        await event.reply(f"Blacklist: {users}")
+
+# --- LANCEMENT ---
+async def main():
+    await client.start(bot_token=BOT_TOKEN)
+    print("Bot hentai/slap/kiss démarré…")
+    await client.run_until_disconnected()
+
+asyncio.run(main())
